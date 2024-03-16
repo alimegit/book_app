3e95c318722a04d69d6ff324feb2e10ee58ad150
Ali <alisherurinov@68@gmail.com>
2024-03-16 19:44:45 +0500

    Tugadi

--- lib/utils/app_text_style.dart
+++ lib/utils/app_text_style.dart
@@ -10,44 +10,81 @@ class AppTextStyle {
     fontFamily: "Roboto",
     fontWeight: FontWeight.w200,
     fontSize: 14.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
   static  TextStyle robotoLight = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w300,
     fontSize: 14.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
   static  TextStyle robotoRegular = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w400,
     fontSize: 16.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
   static  TextStyle robotoMedium = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w500,
     fontSize: 16.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
   static  TextStyle robotoThinItalic = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w600,
     fontSize: 16.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
 
   static  TextStyle robotoBold = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w700,
     fontSize: 16.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
 
   static  TextStyle robotoBlack = TextStyle(
     fontFamily: "Roboto",
     fontWeight: FontWeight.w800,
     fontSize: 16.w,
-    color: AppColors.c_2A3256,
+    color: AppColors.c2A3256,
   );
+  static  TextStyle playFairBlack = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w300,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+  static  TextStyle playFairBlackItalic = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w300,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+  static  TextStyle playFairBold = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w800,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+  static  TextStyle playFairBoldItalic = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w500,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+  static  TextStyle playFairItalic = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w500,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+  static  TextStyle playFairRegular = TextStyle(
+    fontFamily: "PlayfairDisplaySC",
+    fontWeight: FontWeight.w500,
+    fontSize: 16.w,
+    color: AppColors.black,
+  );
+
 }
--- lib/utils/appcolors.dart
+++ lib/utils/appcolors.dart
@@ -7,9 +7,15 @@ class AppColors {
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
 
-  static const Color c_1A72DD = Color(0xFF1A72DD);
-  static const Color c_F4261A = Color(0xFFF4261A);
-  static const Color c_2A3256 = Color(0xFF2A3256);
-  static const Color c_C4C4C4= Color(0xFFC4C4C4);
-  static const Color c_e76f51= Color(0xFFe76f51);
+  static const Color c1A72DD = Color(0xFF1A72DD);
+  static const Color cF4261A = Color(0xFFF4261A);
+  static const Color c2A3256 = Color(0xFF2A3256);
+  static const Color cC4C4C4= Color(0xFFC4C4C4);
+  static const Color ce76f51= Color(0xFFe76f51);
+  static const Color cF8F8F8= Color(0xFFF8F8F8);
+  static const Color c1E1E1E= Color(0xFF1E1E1E);
+  static const Color c939393= Color(0xFF939393);
+  static const Color c404066= Color(0xFF404066);
+  static const Color cCBF5FF= Color(0xFFCBF5FF);
+  static const Color cF5EABD= Color(0xFFF5EABD);
 }
